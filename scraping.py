ff40bbffea591c8c3a129cf12b156ce3dc3b003d
haghiro <nauticatube@yahoo.co.jp>
2020-10-21 22:37:47 +0900

    final commit

--- scraping.py
+++ scraping.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+
+from assets.database import db_session
+
+from assets.models import Data
 import datetime
 
 
@@ -28,17 +31,19 @@ def get_ud_info():
     return results
 
 def write_data():
-    df = pd.read_csv('assets/data.csv')
+
     #新規のデータ
     _results = get_ud_info()
+
     #書き込みのデータ
-    date = datetime.datetime.today().strftime('%Y/%-m/%-d')
+    date = datetime.date.today()
     subscribers = _results['n_subscribers']
     reviews = _results['n_reviews']
-    results = pd.DataFrame([[date, subscribers, reviews]], columns=['date', 'subscribers', 'reviews'])
 
-    df = pd.concat([df, results])
-    df.to_csv('assets/data.csv', index=False)
+    row = Data(date=date, subscribers=subscribers, reviews=reviews)
+
+    db_session.add(row)
+    db_session.commit()
 
 if __name__ == "__main__":
     write_data()
